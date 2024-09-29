48554ce59d0a695e58b9f980b13f7d680f5b91dd
liuzt50906 <liuzt50906@hundsun.com>
2024-09-29 10:51:25 +0800

    🔧 build: 编译报错

--- projects/demo/src/vite-env.d.ts
+++ projects/demo/src/vite-env.d.ts
@@ -1,7 +1,20 @@
 /// <reference types="vite/client" />
 
+import { VNode } from 'vue';
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+
+
+declare type VueNode = VNode;
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elem: string]: unknown;
+    }
+  }
 }
